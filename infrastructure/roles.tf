4c1bd849365991ab11d4fd312cc367fe899dca36
briandenicola <brian@briandenicola.com>
2025-10-02 15:17:05 -0500

    ⚾ Thu Oct  2 15:17:05 CDT 2025

--- infrastructure/roles.tf
+++ infrastructure/roles.tf
@@ -4,13 +4,6 @@ resource "azurerm_role_assignment" "ai_services_user" {
   principal_id                     = data.azurerm_client_config.current.object_id
 }
 
-
-resource "azurerm_role_assignment" "key_user" {
-  scope                            = azurerm_key_vault.this.id
-  role_definition_name             = "Key Vault Crypto Officer"
-  principal_id                     = data.azurerm_client_config.current.object_id 
-}
-
 resource "azurerm_role_assignment" "storage_owner" {
   scope                            = azurerm_storage_account.this.id
   role_definition_name             = "Storage Blob Data Owner"
